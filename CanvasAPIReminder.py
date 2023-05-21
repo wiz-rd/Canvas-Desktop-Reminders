d8d12c4882d1a0059b15b467256f6978a1d98499
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-05-21 15:46:42 -0400

    (True) Initial Commit
    Adding all necessary files and information. Putting "PERSONALinfo.txt" for bug testing.

---
+++ CanvasAPIReminder.py
@@ -0,0 +1,25 @@
+# Canvas API Reminder Project - @wiz-rd
+
+# reading in the API key and domain to use
+with open("info.txt", "r+") as info:
+    # initializing API and domain info from file
+    global api
+    global domain
+    api = info.readline()
+    domain = info.readline()
+
+    # removing newlines
+    api = api.strip()
+    domain = domain.strip()
+
+    if (api == "<API-KEY>" or domain == "<DOMAIN>"):
+        api = input("Please enter your API key: ")
+        domain = input("Please enter your school's domain: ")
+
+    # for debugging and clarity
+    print(api)
+    print(domain)
+
+    # moving to the beginning of the file and then writing the information in
+    info.seek(0)
+    info.write(f"{api}\n{domain}\n# Enter your API key on the first line, and your school's Canvas domain for the second line, or follow the prompts in the program window")
