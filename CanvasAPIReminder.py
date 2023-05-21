9c36fa614d764b300e24d1e235123ab4a34d558c
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-05-21 19:28:22 -0400

    Changing from "calendar_events" to "assignments"
    Discovered why I kept receiving [] empty sets from Canvas API calls; I was requesting the wrong information (calendar_events) so now I'm going to try "assignments"

--- CanvasAPIReminder.py
+++ CanvasAPIReminder.py
@@ -10,49 +10,78 @@ from pathlib import Path
 # constants
 PATH = Path("crm.ico").resolve()
 
+# ensuring the files exist. This seems to be the only way to create files in Python..? All I could find at least
+try:
+    f = open("info.txt", "x")
+    f.close()
+except:
+    print("info.txt already exists")
+try:
+    f = open("response.json", "x")
+    f.close()
+except:
+    print("response.txt already exists")
+
 # functions
 def notify(assignment, dueDate, course):
     
-    timeUntil = 1 # will change this to calculate a day count, potentially
+    today = time.time()
+    timeUntil = today - time.time() # will change this to calculate a day count, potentially
 
     toast = Toast()
-    toast.setTitle(f"An Assignment Will Be Due Soon!", maxLines=1)
+    toast.setTitle(f"{course} Assignment Due Soon!", maxLines=1)
     toast.setMessage(f"{assignment} will be due on {dueDate} for {course}!", maxLines=4)
     toast.setIcon(str(PATH), crop="circle")
     toast.show()
 
-# reading in the API key and domain to use
-with open("info.txt", "r+") as info:
-    # initializing API and domain info from file
-    global api
-    global domain
-    api = info.readline()
-    domain = info.readline()
+def callAPI(courseID):
+    # 604,800 seconds is a week
+    today = datetime.datetime.fromtimestamp((time.time())).strftime("%Y-%m-%d")
+    aWeekFromToday = datetime.datetime.fromtimestamp((time.time() + 604800)).isoformat() #.strftime("%Y-%m-%d")
 
-    # removing newlines
-    api = api.strip()
-    domain = domain.strip()
+    # debugging
+    print(f"{today}\n{aWeekFromToday}")
 
-    if (api == "<API-KEY>" or domain == "<DOMAIN>"):
-        api = input("Please enter your API key: ")
-        domain = input("Please enter your school's domain: ")
+    link = domain + "api/v1/courses/" + courseID + "/assignments" + "?access_token=" + api
 
-    # for debugging and clarity
-    print(api)
-    print(domain)
+    jsonData = {
+        "end_at": aWeekFromToday
+    }
 
-    # moving to the beginning of the file and then writing the information in
-    info.seek(0)
-    info.write(f"{api}\n{domain}\n# Enter your API key on the first line, and your school's Canvas domain for the second line, or follow the prompts in the program window")
+    response = requests.get(link, data=jsonData, verify=True)
+
+    return str(response.json())
+
+def mainProcess():
+    # reading in the API key and domain to use
+    with open("PERSONALinfo.txt", "r+") as info:
+        # initializing API and domain info from file
+        global api
+        global domain
+        api = info.readline()
+        domain = info.readline()
+
+        # removing newlines
+        api = api.strip()
+        domain = domain.strip()
+
+        if (api == "<API-KEY>" or domain == "<DOMAIN>" or api == "" or domain == ""):
+            api = input("Please enter your API key: ")
+            domain = input("Please enter your school's domain: ")
+
+        # for debugging and clarity
+        print(f"API Key: {api[0:20]}...")
+        print(f"Domain: {domain}")
+
+        # moving to the beginning of the file and then writing the information in
+        info.seek(0)
+        info.write(f"{api}\n{domain}\n# Enter your API key on the first line, and your school's Canvas domain for the second line, or follow the prompts in the program window")
+
+    with open("response.json", "r+") as response:
+        response.seek(0)
+        response.write(callAPI())
+
+mainProcess()
 
 # debugging
 #notify("Chess Board Creation", "2/22", "Chess Class")
-
-aWeekFromToday = datetime.datetime.fromtimestamp(int(time.time())).strftime("%Y:%m:%d")
-
-files = {
-    'type': (None, 'assignment'),
-    'end_date': (None, aWeekFromToday),
-}
-
-#response = requests.get('https://<canvas>/api/v1/calendar_events.json', files=files)
